2f0022b92d727d832f58945a62cc68ba7ac70719
Rubikoid <rubikoid@rubikoid.ru>
2023-08-16 19:29:44 +0300

    Make auth.py clean (without err/critical warns)

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,5 @@
 import uuid
-from datetime import datetime, timedelta
-from typing import Optional, Union
+from datetime import UTC, datetime, timedelta
 
 from fastapi import Depends, HTTPException, Request, status
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
@@ -15,17 +14,19 @@ from .config import settings
 class OAuth2PasswordBearerWithCookie(OAuth2):
     def __init__(
         self,
-        scheme_name: str = None,
-        scopes: dict = None,
+        *,
+        scheme_name: str | None = None,
+        scopes: dict | None = None,
+        description: str | None = None,
         auto_error: bool = True,
-    ):
-        if not scopes:
-            scopes = {}
-        flows = OAuthFlowsModel()  # password={"tokenUrl": tokenUrl, "scopes": scopes}
-        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+    ) -> None:
+        scopes = scopes or {}
 
-    async def __call__(self, request: Request) -> Optional[str]:
-        authorization: Optional[str] = request.cookies.get("access_token")
+        flows = OAuthFlowsModel()  # password={"tokenUrl": tokenUrl, "scopes": scopes}
+        super().__init__(flows=flows, scheme_name=scheme_name, description=description, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> str:
+        authorization = request.cookies.get("access_token")
 
         if not authorization:
             raise HTTPException(
@@ -48,48 +49,51 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
 oauth2_scheme = OAuth2PasswordBearerWithCookie()
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)) -> str:
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    expire = datetime.now(tz=UTC) + expires_delta
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
-    return encoded_jwt
+
+    return jwt.encode(to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
 
 
-def create_user_token(user: schema.User):
+def create_user_token(user: schema.User) -> str:
     access_token_expires = timedelta(minutes=settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"user_id": str(user.user_id)}, expires_delta=access_token_expires)
-    return access_token
+    return create_access_token(
+        data={"user_id": str(user.user_id)},
+        expires_delta=access_token_expires,
+    )
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> schema.User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    user_id: Union[str, uuid.UUID, None] = None
+
+    user_id: str | None = None
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])  # no "alg:none"
         user_id = payload.get("user_id")
         if user_id is None:
             raise credentials_exception
-    except JWTError:
-        raise credentials_exception
-    user_id = uuid.UUID(user_id)
-    user = await db.get_user_uuid(uuid=user_id)
+    except JWTError as ex:
+        raise credentials_exception from ex
+
+    user = await db.get_user_uuid(uuid=uuid.UUID(user_id))
     if user is None:
         raise credentials_exception
+
     return user
 
 
-async def get_current_user_safe(request: Request) -> Optional[schema.User]:
+async def get_current_user_safe(request: Request) -> schema.User | None:
     user = None
     try:
         user = await get_current_user(await oauth2_scheme(request))
     except HTTPException:
         user = None
+
     return user

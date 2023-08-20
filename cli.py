73244ecead6b269f2786986bf4be8edaa4e35135
Rubikoid <rubikoid@rubikoid.ru>
2023-08-21 01:49:38 +0300

    CLI updates

--- cli.py
+++ cli.py
@@ -1,5 +1,4 @@
 import asyncio
-import pickle
 import random
 import shutil
 import string
@@ -13,10 +12,10 @@ from types import TracebackType
 
 import httpx
 import typer
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
 from rich.console import Console
 
-from app import app, config, schema
+from app import app, auth, config, schema
 
 
 class UserPublic(schema.EBaseModel):
@@ -69,8 +68,13 @@ class FileTask(BaseModel):
         )
 
 
+AllUsers = RootModel[dict[uuid.UUID, UserPrivate]]
+AllTasks = RootModel[dict[uuid.UUID, schema.Task]]
+
 files_domain = "http://127.0.0.1:9999"
-base_url = "http://127.0.0.1:8000"
+base_url = "http://127.0.0.1:8080"
+FLAG_BASE = "kks"
+
 
 tapp = typer.Typer()
 c = Console()
@@ -85,49 +89,55 @@ class YATB:
     def set_admin_token(self, token: str) -> None:
         self.s.headers["X-Token"] = token
 
-    async def register_user(self, user: RawUser) -> None:
+    def make_user_token(self, user: schema.User) -> str:
+        return f"Bearer {auth.create_user_token(user)}"
+
+    async def register_user(self, user: RawUser) -> UserPrivate:
         resp = await self.s.post(
             app.url_path_for("api_auth_simple_register"),
             json=schema.SimpleAuth.Form._Internal(
                 username=user.username,
                 password=user.password,
-            ).dict(),
+            ).model_dump(mode="json"),
         )
         resp.raise_for_status()
 
+        ret = await self.find_user_by_name(user.username)
+        if not ret:
+            raise Exception("WTF")
+        return ret
+
     async def get_self(self) -> UserPublic:
-        return UserPublic.parse_obj((await self.s.get(app.url_path_for("api_users_me"))).json())
+        return UserPublic.model_validate((await self.s.get(app.url_path_for("api_users_me"))).json())
 
     async def get_all_tasks(self) -> dict[uuid.UUID, schema.Task]:
-        class AllTasks(schema.EBaseModel):
-            __root__: dict[uuid.UUID, schema.Task]
-
-        resp = AllTasks.parse_obj((await self.s.get(app.url_path_for("api_admin_tasks"))).json())
-        return resp.__root__
+        resp = AllTasks.model_validate((await self.s.get(app.url_path_for("api_admin_tasks"))).json())
+        return resp.root
 
     async def get_all_users(self) -> dict[uuid.UUID, UserPrivate]:
-        class Allusers(schema.EBaseModel):
-            __root__: dict[uuid.UUID, UserPrivate]
-
-        resp = Allusers.parse_obj((await self.s.get(app.url_path_for("api_admin_users"))).json())
-        return resp.__root__
+        resp = AllUsers.model_validate((await self.s.get(app.url_path_for("api_admin_users"))).json())
+        return resp.root
 
     async def assign_task_to_user(self, user_id: uuid.UUID, task_id: uuid.UUID) -> UserPrivate:
         resp = await self.s.post(
             app.url_path_for("api_admin_assign_task_to_user", user_id=user_id),
             params={"task_id": str(task_id)},
         )
-        return UserPrivate.parse_obj(resp.json())
+        return UserPrivate.model_validate(resp.json())
 
     async def deassign_task_to_user(self, user_id: uuid.UUID, task_id: uuid.UUID) -> UserPrivate:
         resp = await self.s.post(
             app.url_path_for("api_admin_deassign_task_to_user", user_id=user_id),
             params={"task_id": str(task_id)},
         )
-        return UserPrivate.parse_obj(resp.json())
+        return UserPrivate.model_validate(resp.json())
 
     async def detele_everything_but_tasks(self):
-        resp = await self.s.get(app.url_path_for("api_detele_everything_but_tasks"))
+        resp = await self.s.delete(app.url_path_for("api_detele_everything_but_tasks"))
+        resp.raise_for_status()
+
+    async def detele_everything(self):
+        resp = await self.s.delete(app.url_path_for("api_detele_everything"))
         resp.raise_for_status()
 
     async def find_user_by_name(self, username: str) -> UserPrivate | None:
@@ -154,22 +164,36 @@ class YATB:
                     task_name=task.task_name,
                     description=task.description,
                     category=task.category,
-                    flag=schema.flags.StaticFlag(flag=task.flag, flag_base="Cup"),
+                    flag=schema.flags.StaticFlag(flag=task.flag, flag_base=FLAG_BASE),
                     scoring=schema.scoring.DynamicKKSScoring(),
                     author=task.author,
-                ).dict(),
+                ).model_dump(mode="json"),
             )
         ).json()
-        return schema.Task.parse_obj(new_task)
+        return schema.Task.model_validate(new_task)
+
+    async def admin_recalc_scoreboard(self) -> None:
+        resp = await self.s.get(app.url_path_for("api_admin_recalc_scoreboard"))
+        resp.raise_for_status()
 
     async def update_task(self, task: schema.Task) -> schema.Task:
         new_task = (
             await self.s.post(
                 app.url_path_for("api_admin_task_edit", task_id=task.task_id),
-                data=task.json(),  # httpx shit ;(, sry
+                json=task.model_dump(mode="json"),
             )
         ).json()
-        return schema.Task.parse_obj(new_task)
+        return schema.Task.model_validate(new_task)
+
+    async def solve_as_user(self, user: schema.User, flag: str) -> str:
+        token = self.make_user_token(user)
+        resp = await self.s.post(
+            app.url_path_for("api_task_submit_flag"),
+            json=schema.FlagForm(flag=flag).model_dump(mode="json"),
+            headers={"X-Auth-Token": token},
+        )
+        resp.raise_for_status()
+        return resp.text
 
     async def __aenter__(self):
         self.s = await self.s.__aenter__()
@@ -181,7 +205,7 @@ class YATB:
         exc_value: BaseException | None = None,
         traceback: TracebackType | None = None,
     ) -> None:
-        await self.s.__aexit__(exc_type=exc_type, exc_value=exc_value, traceback=traceback)  # type: ignore
+        await self.s.__aexit__(exc_type=exc_type, exc_value=exc_value, traceback=traceback)
 
 
 tasks_to_create: list[RawTask] = [
@@ -225,7 +249,7 @@ tasks_to_create: list[RawTask] = [
 
 
 @tapp.command()
-def get_all_tasks():  # noqa: CCR001,ANN201
+def get_all_tasks():  # noqa: ANN201
     async def _a():
         async with YATB() as y:
             y.set_admin_token(config.settings.API_TOKEN)
@@ -248,7 +272,7 @@ def get_all_tasks():  # noqa: CCR001,ANN201
 
 
 @tapp.command()
-def get_all_users():  # noqa: CCR001,ANN201
+def get_all_users():  # noqa: ANN201
     async def _a():
         async with YATB() as y:
             y.set_admin_token(config.settings.API_TOKEN)
@@ -269,7 +293,7 @@ def get_all_users():  # noqa: CCR001,ANN201
 
 
 @tapp.command()
-def drop_users():  # noqa: CCR001,ANN201
+def drop_users():  # noqa: ANN201
     shure = typer.prompt("Are you shure? [y/N]", default="N")
     if shure not in ["y", "yes"]:
         return
@@ -283,7 +307,7 @@ def drop_users():  # noqa: CCR001,ANN201
 
 
 @tapp.command()
-def init_tasks():
+def init_tasks():  # noqa: ANN201
     async def _a():
         async with YATB() as y:
             y.set_admin_token(config.settings.API_TOKEN)
@@ -291,6 +315,90 @@ def init_tasks():
             for task in tasks_to_create:
                 new_task = await y.create_task(task)
                 c.log(f"Task created: {new_task = }")
+
+    asyncio.run(_a())
+
+
+@tapp.command()
+def cleanup():  # noqa: ANN201
+    async def _a():
+        async with YATB() as y:
+            y.set_admin_token(config.settings.API_TOKEN)
+            await y.detele_everything()
+
+    asyncio.run(_a())
+
+
+@tapp.command()
+def recalc():
+    async def _a():
+        async with YATB() as y:
+            y.set_admin_token(config.settings.API_TOKEN)
+            await y.admin_recalc_scoreboard()
+
+    asyncio.run(_a())
+
+
+@tapp.command()
+def setup_test_env(*, heavy: bool = False):  # noqa: ANN201
+    total = 500 if heavy else 6
+    lim1 = 200 if heavy else 6
+    lim2 = 300 if heavy else 6
+    lim3 = 350 if heavy else 6
+
+    users_to_create = [
+        RawUser(
+            username=f"test_user_{i}",
+            password="1",  # noqa: S106
+        )
+        for i in range(total)
+    ]
+
+    async def _a():
+        async with YATB() as y:
+            y.set_admin_token(config.settings.API_TOKEN)
+            await y.detele_everything()
+
+            users = {}
+
+            for task in tasks_to_create:
+                new_task = await y.create_task(task)
+                new_task.hidden = False
+                await y.update_task(new_task)
+                c.log(f"Task created: {new_task = }")
+
+            for i, user in enumerate(users_to_create):
+                users[i] = await y.register_user(user)
+
+            for i in range(0, lim1, 2):
+                await y.solve_as_user(users[i], "A")
+                await y.solve_as_user(users[i], "B")
+
+            for i in range(0, lim2, 3):
+                await y.solve_as_user(users[i], "B")
+                await y.solve_as_user(users[i], "C")
+
+            for i in range(0, lim3, 5):
+                await y.solve_as_user(users[i], "As")
+
+    asyncio.run(_a())
+
+
+@tapp.command()
+def make_load(url: str):
+    async def _a():
+        async with httpx.AsyncClient() as c:
+
+            async def task():
+                print("Start")
+                for _ in range(0, 1000):
+                    r = await c.get(url)
+                    _ = r.text
+                print("End")
+
+            async with asyncio.TaskGroup() as tg:
+                for _ in range(0, 10):
+                    tg.create_task(task())
 
     asyncio.run(_a())
 

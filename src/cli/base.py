4a62560c460566aeed0b0532a3e0e64465cea7b1
Rubikoid <rubikoid@rubikoid.ru>
2025-08-17 02:40:05 +0300

    mv src/yatb/cli -> src/cli

--- src/cli/base.py
+++ src/cli/base.py
@@ -1,10 +1,16 @@
 import cyclopts
 import rich.console
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     server: str = "http://127.0.0.1:8000"
+
+    model_config = SettingsConfigDict(
+        env_file="yatb.env",
+        env_file_encoding="utf-8",
+        extra="allow",
+    )
 
 
 settings = Settings()  # pyright: ignore[reportCallIssue]
